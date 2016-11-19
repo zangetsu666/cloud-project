5ecec04e63b445fdcfeaedb073383bdbcf47646d
drAppleCo <warlock.skt@gmail.com>
2016-11-19 15:20:20 +0100

    First commit

---
+++ cloud-project/src/main/java/ad/pyjamasteam/CloudProjectApplication.java
@@ -0,0 +1,12 @@
+package ad.pyjamasteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudProjectApplication.class, args);
+	}
+}
